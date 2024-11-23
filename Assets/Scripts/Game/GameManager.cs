3d1619f96c75c99597246eaf6c899b9aafe0eef5
Vernandio <vernandio.rivaldo@gmail.com>
2024-11-23 22:48:35 +0700

    Escape Menu + Message game over

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -14,6 +15,7 @@ public class GameManager : MonoBehaviour
     public Slider playerHPBar;
     public Slider playerExpBar;
     public PlayerDataSO playerData;
+    public GameObject escapeMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -34,6 +36,30 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            bool isActive = escapeMenu.activeSelf;
+            escapeMenu.SetActive(!isActive); // Toggle active state
+        }
+
+        if (escapeMenu.activeSelf)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+
+    public void resume(){
+        escapeMenu.SetActive(false);
+    }
+
+    public void backToUpgradeMenu(){
+        SceneManager.LoadScene("Upgrade Menu");
+    }
+
+    public void backToMainMenu(){
+        SceneManager.LoadScene("Main Menu");
     }
 }
