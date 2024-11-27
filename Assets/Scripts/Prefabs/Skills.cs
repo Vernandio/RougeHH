584676a0115547e785efb2ff307002863c2ef4af
Vernandio <vernandio.rivaldo@gmail.com>
2024-11-27 22:44:58 +0700

    Hover skill

--- Assets/Scripts/Prefabs/Skills.cs
+++ Assets/Scripts/Prefabs/Skills.cs
@@ -10,11 +10,14 @@ public class Skills : MonoBehaviour
     public PlayerDataSO playerData;
     public Image skillImage;
     string imageName;
+    public GameObject hover;
+    public Text hoverText;
 
     // Start is called before the first frame update
     void Start()
     {
         imageName = skillImage.sprite.name;
+        hover.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,5 +31,48 @@ public class Skills : MonoBehaviour
         if((playerData.playerLevel - 2) >= int.Parse(slot.text) || imageName == "Mini_frame0"){
             locked.SetActive(false);
         }
+    }
+
+    // Method to handle mouse enter event for hover effect
+    public void OnMouseEnter(string type)
+    {
+        hover.SetActive(true);
+        if(type == "Locked"){
+            switch(imageName){
+                case "16_life_drain":
+                    hoverText.text = "Unlocked at level 3";
+                    break;
+                case "09_Melee_slash":
+                    hoverText.text = "Unlocked at level 4";
+                    break;
+                case "13_Healing_spell_2":
+                    hoverText.text = "Unlocked at level 5";
+                    break;
+                default:
+                    hover.SetActive(false);
+                    break;
+            }
+        }else{
+            switch(imageName){
+                case "16_life_drain":
+                    hoverText.text = "Life Steal - For each successfull hit, increase your health by 20% of your attack";
+                    break;
+                case "09_Melee_slash":
+                    hoverText.text = "Bash - Unleash powerful strike to the head, dealing 150% of your attack as physical damage";
+                    break;
+                case "13_Healing_spell_2":
+                    hoverText.text = "+1000 Aura - Increase your aura by 100, Improve your attack and defend 20$";
+                    break;
+                default:
+                    hover.SetActive(false);
+                    break;
+            }
+        }
+    }
+
+    // Method to handle mouse exit event for hover effect
+    public void OnMouseExit()
+    {
+        hover.SetActive(false);
     }
 }
